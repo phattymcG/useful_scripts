daf2613c90adc6fc418cdfdb286075c4786e7dfe
phattymcG <46428218+phattymcG@users.noreply.github.com>
2019-04-05 07:03:40 +0000

    Used with one-liner SMTP scan

---
+++ vrfy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import socket
+import sys
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect=s.connect(('10.11.1.215',25))
+banner=s.recv(1024)
+print banner
+s.send('VRFY ' + sys.argv[1] + '\r\n')
+result=s.recv(1024)
+print result
+s.close()
